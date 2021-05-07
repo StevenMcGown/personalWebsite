2c0da735cbec5de7502dc89e292a177b4995d31d
StevenMcGown <stevenmcgown1@gmail.com>
2021-05-07 10:53:38 -0400

    changed everything - just resume now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ArtComponent } from './art/art.component';
-import { BlogComponent } from './blog/blog.component';
 import { HomeComponent } from './home/home.component';
-import { ReadingsComponent } from './readings/readings.component';
-import { ResumeComponent } from './resume/resume.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { ForgotComponent } from './forgot/forgot.component';
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
-  {path:'home',component:HomeComponent},
-  {path:'resume',component:ResumeComponent},
-  {path:'blog',component:BlogComponent},
-  {path:'art',component:ArtComponent},
-  {path:'readings',component:ReadingsComponent},
-  {path:'signIn',component:SignInComponent},
-  {path:'signUp',component:SignUpComponent},
-  {path:'forgot',component:ForgotComponent}
+  {path:'home',component:HomeComponent}
 ];
 
 @NgModule({
