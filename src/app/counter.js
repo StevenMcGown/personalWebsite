8876211345c179c567330a3c19f07ca71d302908
StevenMcGown <stevenmcgown1@gmail.com>
2021-06-22 19:16:57 -0400

    test

--- src/app/counter.js
+++ src/app/counter.js
@@ -1,45 +1,24 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-east-1'});
 
-/**
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * This file is licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License. A copy of
- * the License is located at
- *
- * http://aws.amazon.com/apache2.0/
- *
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
-*/
-var AWS = require("aws-sdk");
-
-AWS.config.update({
-  region: "us-east-1",
-  endpoint: "dynamodb.us-east-1.amazonaws.com"
-});
-
-var docClient = new AWS.DynamoDB.DocumentClient();
-
-var table = "counter";
-
-var num_visits = 0;
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 var params = {
-    TableName:table,
-    Item:{
-        "year": num_visits,
-        "info":{
-            "Description": "Number of visits to the resume"
-        }
-    }
+  TableName: 'CUSTOMER_LIST',
+  Item: {
+    'CUSTOMER_ID' : {N: '001'},
+    'CUSTOMER_NAME' : {S: 'Richard Roe'}
+  }
 };
 
-console.log("Adding a new item...");
-docClient.put(params, function(err, data) {
-    if (err) {
-        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Added item:", JSON.stringify(data, null, 2));
-    }
+// Call DynamoDB to add the item to the table
+ddb.putItem(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
 });
