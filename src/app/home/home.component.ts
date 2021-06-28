3f7d9d71b027ea2804e2884a46637e3d9a8400d7
StevenMcGown <stevenmcgown1@gmail.com>
2021-06-28 11:03:22 -0400

    displayed number of visitors on page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,5 @@
 import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-<<<<<<< HEAD
-var Count = 123;
-function getCount(){
-  var response = fetch('https://9evudan1v4.execute-api.us-east-1.amazonaws.com/Prod/hello/');
-  return response;
-}
-=======
 import { HttpClient } from '@angular/common/http';
->>>>>>> da32b4537834c000a5187342940d44913011d5af
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -17,12 +7,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent {
 
-  visits = 0;
-
+visitors = "";
   constructor(private http: HttpClient) {    
     this.http.get('https://9evudan1v4.execute-api.us-east-1.amazonaws.com/Prod/hello/').subscribe(data =>{
       console.log(data);
-      this.visits = data['Visitors'];
+      const visitors = JSON.stringify(data).split("\"");
+      this.visitors = visitors[3];
     });
   }
 
