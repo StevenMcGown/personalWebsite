da32b4537834c000a5187342940d44913011d5af
StevenMcGown <stevenmcgown1@gmail.com>
2021-06-28 00:25:21 -0400

    connected dynamodb fetch to typescript to html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,7 @@ export class HomeComponent {
   constructor(private http: HttpClient) {    
     this.http.get('https://9evudan1v4.execute-api.us-east-1.amazonaws.com/Prod/hello/').subscribe(data =>{
       console.log(data);
+      this.visits = data['Visitors'];
     });
   }
 
