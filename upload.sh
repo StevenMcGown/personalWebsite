34da1cb25c2a2a13cf9c46a315b10bfdc0847a93
Steven McGown <stevenmcgown1@gmail.com>
2023-03-18 12:56:24 -0400

    re-rerunning the workflow bc I goofed

--- upload.sh
+++ upload.sh
@@ -2,4 +2,3 @@ ng build
 aws s3 rm s3://smcgown.com --recursive
 aws s3 sync ./dist/CloudResumeChallenge2/ s3://smcgown.com --acl public-read
 aws cloudfront create-invalidation --distribution-id EO37R0PS8J6V4 --paths "/*"
-
